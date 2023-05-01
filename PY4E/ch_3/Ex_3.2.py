16ec38be165bc7bfa077ee09678b31a8fbba2616
sytrejo <s.y.trejo26@gmail.com>
2023-05-01 17:59:31 -0400

    updates

--- PY4E/ch_3/Ex_3.2.py
+++ PY4E/ch_3/Ex_3.2.py
@@ -7,3 +7,23 @@
 # >= 0.6 D
 # < 0.6 F
 # If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+
+scoreInput = input("Enter Score: ")
+
+try:
+    score = float(scoreInput)
+except:
+    print("Error, please enter numeric input")
+    quit()
+    
+    
+if score >= 0.9 :
+   print("A")
+elif score >= 0.8 :
+   print("B")
+elif score >= 0.7 :
+   print("C")
+elif score >= 0.6 :
+   print("D")
+elif score < 0.6 :
+   print("F")
